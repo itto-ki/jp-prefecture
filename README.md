fb4f556dbacb3be5481a8428a39945f125400b21
Ittoh Kimura <kimura.itto.kd3@gmail.com>
2022-04-08 00:50:04 +0900

    Docs: add sections to Cargo.toml

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # jp-prefecture
+
+jp-prefecture is an useful crate for using Japanese prefectures
+
+# Getting Started
+
+Crate has to be added as dependency to `Cargo.toml`
+
+```toml
+[dependencies]
+jp-prefecture = "0.1.0"
+```
+
+and imported to the scope of a block where it's begin called
+
+```rust
+use jp_prefecture::prefectures;
+```
+
+# Examples
+
+```rust
+use jp_prefecture::prefectures;
+
+let tokyo = prefectures::find_by_kanji("東京都");
+println!("{:?}", tokyo); // => Some(Prefecture::Tokyo)
+println!("{:?}", tokyo.unwrap().kanji()); // => "東京都"
+println!("{:?}", tokyo.unwrap().kanji_short()); // => "東京"
+println!("{:?}", tokyo.unwrap().english()); // => "tokyo"
+
+let tokyo = prefectures::find_by_hiragana("東京県"); // uhmmmm...
+println!("{:?}", tokyo); // => None
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! # Overview
-//! jp-prefecture is utility library for using Japanese prefectures
+//! jp-prefecture is an utility library for using Japanese prefectures
 //!
 //! # Getting Started
 //! Crate has to be added as dependency to `Cargo.toml`
@@ -17,9 +17,12 @@
 //! use jp_prefecture::prefectures;
 //!
 //! let tokyo = prefectures::find_by_kanji("東京都");
+//! println!("{:?}", tokyo); // => Some(Prefecture::Tokyo)
+//! println!("{:?}", tokyo.unwrap().kanji()); // => "東京都"
+//! println!("{:?}", tokyo.unwrap().kanji_short()); // => "東京"
 //! println!("{:?}", tokyo.unwrap().english()); // => "tokyo"
 //!
-//! let tokyo = prefectures::find_by_hiragana("とうきょうけん"); // uhmmmm...
+//! let tokyo = prefectures::find_by_hiragana("東京県"); // uhmmmm...
 //! println!("{:?}", tokyo); // => None
 //! ```
 
